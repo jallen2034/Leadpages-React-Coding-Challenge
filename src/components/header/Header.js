0463fda993cdd847d0ce1a3aa231e642cc9e0e9f
Jacob Allen <jallen2033@gmail.com>
2023-04-27 15:52:02 -0700

    AMN-002 - Added feature to show snackbar notifiations for users which are created

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,4 @@
-import React, {useEffect, useState} from 'react';
-
+import React from 'react';
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -7,24 +6,65 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import Snackbar from '@mui/material/Snackbar';
 import {
   createMockFormSubmission,
 } from '../../service/mockServer';
 import {submitForm} from "./helpers";
 
-export default function Header() {
+export default function Header(props) {
+  const { setPostUpdated } = props;
+  const [openError, setOpenError] = React.useState(false);
 
   /* This function is triggered when the user clicks on the 'New Submission' button.
    * It calls the submitForm() function, passing in the createMockFormSubmission callback
    * function, which generates random data (first name, last name, email, and likes)
    * for the new submission. The submission is then sent to the server via this callback. */
   const handleSubmitNewMessage = async (event) => {
-    event.preventDefault();
-    await submitForm(createMockFormSubmission);
+    try {
+      event.preventDefault();
+      await submitForm(createMockFormSubmission);
+      setPostUpdated(true);
+    } catch (error) {
+      console.error(error);
+      setOpenError(true);
+    }
   }
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpenError(false);
+  };
+
+  const action = (
+    <React.Fragment>
+      <Button color="secondary" size="small" onClick={handleClose}>
+        UNDO
+      </Button>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
 
   return (
     <Box sx={{flexGrow: 1}}>
+      <Snackbar
+        open={openError}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message="Something went wrong when submitting a post, please try again"
+        action={action}
+      />
       <AppBar position="static">
         <Toolbar>
           <IconButton
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Container from '@mui/material/Container';
 
 import Header from '../components/header/Header';
 import Content from '../components/content/Content';
 
 function App() {
+  const [postUpdated, setPostUpdated] = useState(false);
   return (
     <>
-      <Header />
+      <Header setPostUpdated={setPostUpdated} />
       <Container>
-        <Content />
+        <Content
+          postUpdated={postUpdated}
+          setPostUpdated={setPostUpdated}
+        />
       </Container>
     </>
   );
