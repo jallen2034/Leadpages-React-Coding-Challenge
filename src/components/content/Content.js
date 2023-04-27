51a815ac922d0e98a30ca1a74f63a2410ba8a9b9
Jacob Allen <jallen2033@gmail.com>
2023-04-27 14:36:31 -0700

    AMN-001 - Implmented first feature that allows a randomly generated form submission to be created and sent to our API when a user clicks on the 'new submission' button

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -4,15 +4,6 @@ import Typography from '@mui/material/Typography';
 import {fetchLikedFormSubmissions} from "../../service/mockServer";
 
 export default function Content() {
-
-  // useEffect(() => {
-  //   const fetchLikes = async () => {
-  //     const likes = await fetchLikedFormSubmissions();
-  //     console.log("likes", likes);
-  //   }
-  //   fetchLikes();
-  // }, [])
-
   return (
     <Box sx={{marginTop: 3}}>
       <Typography variant="h4">Liked Form Submissions</Typography>
