da2be1174abe11aa400f0d70a6215a867c11ed78
Jacob Allen <jallen2033@gmail.com>
2023-04-27 18:14:41 -0700

    Fixed bug with method to fetch up to date posts crashing the page

--- src/components/content/helpers.js
+++ src/components/content/helpers.js
@@ -5,9 +5,11 @@ export const fetchUpToDatePosts = async (
   setPostUpdated,
   setOpen
 ) => {
-  const updatedFormSubmissions = await fetchLikedFormSubmissions(); // API call to get Form Submissions
-  if (updatedFormSubmissions.formSubmissions.length > 0) { // Safety check
-    setMostRecentSubmission(updatedFormSubmissions.formSubmissions[updatedFormSubmissions.length - 1]);
+  const data = await fetchLikedFormSubmissions(); // API call to get Form Submissions
+  if (data.formSubmissions.length > 0) { // Safety check
+    setMostRecentSubmission(
+      data.formSubmissions[data.formSubmissions.length - 1]
+    );
     setPostUpdated(false);
     setOpen(true);
   }
